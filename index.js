50b88d0113231de0b1547aacbc2c6324d067b15f
Sara Muhammad <saramuhammad1505@gmail.com>
2026-02-02 07:30:39 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var weather = prompt (`
+  Select your Fav weather :
+  1-rainy
+  2-sunny
+  3-winter
+  4-autumn
+  `).toLowerCase();
+
+  if(weather === "rainy" ){
+  console.log("rainy");
+  }else if(weather === "sunny"){
+    console.log("sunny");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
